070c4d09d0a635d13de21f7270ef18f2e9c897cc
raghavan <raghavan@krishna.(none)>
2012-12-12 23:34:10 +0530

    An empty rails 3 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveyor::Application.config.secret_token = 'cfd1ef9d0dc22d0d8a44d217e8e4d0391daad1415cdc903153068929913f0e2e60f8b8685073fec8a65f40049cdae2aad3bd4f147818d9c17beb94761ed52dab'
